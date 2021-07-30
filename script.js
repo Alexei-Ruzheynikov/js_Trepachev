d525205ee7c41f4305bcc0a60294a1622f70cfd4
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-07-30 15:24:00 +0300

    3

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-alert(60 * 60);
-alert(60 * 60 * 24);
+// var num = 1;
+// num = num + 12;
+// num = num - 14;
+// num = num * 5;
+// num = num / 7;
+// num = num + 1;
+// num = num - 1;
+// alert(num);
+
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
