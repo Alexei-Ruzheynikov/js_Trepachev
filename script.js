7af3202b410f53a7e72e159b5ec7e34ed314a9c3
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-07-30 16:01:09 +0300

    4

--- script.js
+++ script.js
@@ -1,17 +1,118 @@
-// var num = 1;
-// num = num + 12;
-// num = num - 14;
-// num = num * 5;
-// num = num / 7;
+// var num = 3;
+// alert(num);
+// ___________________
+
+// var a = 10,
+//   b = 2;
+// sum = a + b;
+// alert(sum);
+// dif = a - b;
+// alert(dif);
+// prod = a * b;
+// alert(prod);
+// div = a / b;
+// alert(div);
+// ___________________
+
+// var c = 15,
+//   d = 2;
+// result = c + d;
+// alert(result);
+// ___________________
+
+// var a = 10,
+//   b = 2,
+//   c = 5,
+//   sum;
+// sum = a + b + c;
+// alert(sum);
+// ___________________
+
+// var a = 17,
+//   b = 10,
+//   d = 7;
+// c = a - b;
+// result = d + c;
+// alert(result);
+// ___________________
+
+// var str = "Привет, Мир!";
+// alert(str);
+// ___________________
+
+// var str1 = "Привет, ",
+//   str2 = "Мир!";
+// alert(str1 + str2);
+// ___________________
+
+// var name = "Alex";
+// alert("Привет, " + name + "!");
+// ___________________
+
+// var age = 23;
+// alert("Мне " + age + " года");
+// ___________________
+
+// var name = prompt("Ваше имя?");
+// alert("Ваше имя " + name);
+// ___________________
+
+// var numbers = prompt("Введите число");
+// alert(numbers * numbers);
+// ___________________
+
+// var str = "abcde";
+// alert(str[0]);
+// alert(str[2]);
+// alert(str[4]);
+// ___________________
+
+// var num = "12345";
+// alert(num[0] * num[1] * num[2] * num[3] * num[4]);
+// ___________________
+
+// alert(60 * 60);
+// alert(60 * 60 * 24);
+// alert(60 * 60 * 24 * 30);
+// ___________________
+
+// var hour = 15,
+//   minute = 57,
+//   second = 44;
+// alert(hour + ":" + minute + ":" + second);
+// ___________________
+
+// var a = 5;
+// a = a * a;
+// alert(a);
+// ___________________
+
+// var num = 47;
+// num = num + 7;
+// num = num - 18;
+// num = num * 10;
+// num = num / 15;
+// alert(num);
+// ___________________
+
+// var num = 47;
+// num += 7;
+// num - +18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+// ___________________
+
+// var num = 10;
+// num = num + 1;
 // num = num + 1;
 // num = num - 1;
 // alert(num);
+// ___________________
 
-var num = 1;
-num += 12;
-num -= 14;
-num *= 5;
-num /= 7;
-num++;
-num--;
-alert(num);
+// var num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+// ___________________
