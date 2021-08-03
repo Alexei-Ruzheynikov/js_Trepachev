0301f21c613359d1d61ff9c512661410dc957e72
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-03 21:24:28 +0300

    8

--- script.js
+++ script.js
@@ -1,97 +1,13 @@
-// Теория Работа со строковыми функциями
+// Теория связанная с массивами
 
-// length - длина строки
-// let str = "abcde";
-// console.log(str.length);
-
-// toUpperCase() - производит преобразование строки в верхний регистр
-// let str = "Язык Javascript";
-// console.log(str.toUpperCase());
-
-// toLowerCase() - производит преобразование строки в нижний регистр
-// let str = "Язык JAVASCRIPT";
-// console.log(str.toLowerCase());
-
-//  Метод  substr возвращает подстроку из строки(1 = с какого места отрезать, 2 - сколько)
-// let str = "abcde";
-// let sub = str.substr(0, 3);
-// console.log(sub);
-
-// let str = "abcde";
-// let sub = str.substr(2);
-// console.log(sub);
-
-// let str = "abcde";
-// let sub = str.substr(-3, 2);
-// console.log(sub);
-
-// let str = "abcde";
-// let sub = str.substr(-3);
-// console.log(sub);
-
-// let str = "abcde";
-// let sub = str.substr(-1);
-// console.log(sub);
-
-// substring возвращает подстроку из строки, где 1 символ откуда начать, 2 символ - докуда резать
-// let str = "abcde";
-// let sub = str.substring(1, 3);
-// console.log(sub);
-
-// let str = "abcde";
-// let sub = str.substring(1);
-// console.log(sub);
-
-// substr 1 символ - откуда отрезать, 2 символ - докуда резать
-// let str = "abcde";
-// let sub = str.slice(1, 3);
-// console.log(sub);
-
-// let str = "abcde";
-// let sub = str.slice(1);
-// console.log(sub);
-
-// let str = "abcde";
-// let sub = str.slice(1, -2);
-// console.log(sub);
-
-// Метод indexOf возвращает позицию первого совпадения, где 1 -что ищем, 2 - откуда начать поиск
-// let str = "Я учу учу Javascript";
-// console.log(str.indexOf("учу"));
-
-// let str = "Я учу учу Javascript";
-// console.log(str.indexOf("учу", 5));
-
-// вернет -1 т.к. чувствителен к регистру и при не совпадении выдает -1
-// let str = "Я учу Javascript";
-// console.log(str.indexOf("JavaScript"));
-
-// вернет -1 т.к начиная с 8 символа "учу" больше нет и при не нахождении возвращает -1
-// let str = "Я учу Javascript";
-// console.log(str.indexOf("учу", 8));
-
-// split разбивает строки в массив по указанному разделителю
-
-// let str = "html-css-javascript";
-// let arr = str.split("-");
+// Метод reverse изменяет порядок элементов на обратный
+// let arr = ["a", "b", "c"];
+// arr.reverse();
 // console.log(arr);
 
-// Вторым параметром указывает необходимое количество элементов
-// let str = "html-css-javascript";
-// let arr = str.split("-", 2);
-// console.log(arr);
-
-// let str = "adcde";
-// let arr = str.split("");
-// console.log(arr);
-
-// let str = "abcde";
-// let arr = str.split("", 3);
-// console.log(arr);
-
-// let str = "12345";
-// let arr = str.split("");
-// console.log(arr);
+// let arr = ["a", "b", "c"];
+// let res = arr.reverse();
+// console.log(res);
 
 // let str = "123456789";
 // let arr1 = str.split("");
@@ -103,147 +19,221 @@
 // let result = str.split("").reverse().join("");
 // console.log(result);
 
-// let str = "12345";
-// let arr = str.split("");
-// let sum = 0;
-// for (let i = 0; i < arr.length; i++) {
-//   sum += Number(arr[i]);
-// }
-// console.log(sum);
-
-// let num = 12345;
-// let str = String(num);
-// let arr = str.split("");
+//Метод push добавляет неограниченное количество элементов в конец массива.
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.push("1", "2");
 // console.log(arr);
 
-// Метод join - объединяет элементы массива в стркоу с указанием разделителя
-// let arr = [1, 2, 3];
-// let str = arr.join("-");
-// console.log(str);
+// При такой записи в переменную запишется длина массива
+// let arr = ["a", "b", "c", "d", "e"];
+// let length = arr.push("1", "2");
+// console.log(length);
 
-// По умолчанию разделитель - запятая
-// let arr = [1, 2, 3];
-// let str = arr.join();
-// console.log(str);
+// let arr = [];
+// for (let i = 1; i <= 9; i++) {
+//   arr.push(i);
+// }
+// console.log(arr);
 
-// разделителя нет
-// let arr = [1, 2, 3];
-// let str = arr.join("");
-// console.log(str);
+// Метод unshift добавляет неограниченное количество новых элеменртов в начало массива
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.unshift("1", "2");
+// console.log(arr);
 
-// let date = "2025-12-31";
-// let result = date.split("-").reverse().join(".");
+// Выведет новую длину массива
+// let arr = ["a", "b", "c", "d", "e"];
+// let length = arr.unshift("1", "2");
+// console.log(length);
+
+// Метод shift удаляет первый элемент из массива, а результатом метода возвращается удаленный элемент
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.shift();
+// console.log(arr);
+
+// let arr = ["a", "b", "c", "d", "e"];
+// let elem = arr.shift();
+// console.log(elem);
+
+// let arr = ["1", "2", "3", "4", "5", "6"];
+// let result = [];
+// while (arr.length > 0) {
+//   let first = arr.shift();
+//   let last = arr.pop();
+//   let str = first + last;
+//   result.push(str);
+// }
+// result = result.join("-");
 // console.log(result);
 
-//Функция implode сливает массив в строку с указанным разделителем, но это для php
+// Метод pop  удаляет последний элемент из массива
 
-//Практика
-//Задача 1
-// g - изменит все, '@' - только 1 изменит
-// var str = "aaa@bbb@ccc";
-// alert(str.replace(/@/g, "!"));
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.pop();
+// console.log(arr);
 
-//Задача 2
-// var str = "aaa bbb ccc";
-// //substr(откуда отрезать, сколько отрезать)
-// alert(str.substr(4, 3));
-// // substring(откуда отрезать, докуда отрезать)
-// alert(str.substring(4, 7));
-// //slice(откуда отрезать, дкуда отрезать)
-// alert(str.slice(4, 7));
+// let arr = ["a", "b", "c", "d", "e"];
+// let del = arr.pop();
+// console.log(del);
 
-//Задача 3
-// var date = "2025-12-31";
-// var arr = date.split("-");
+// let arr = ["1", "2", "3", "4", "5", "6"];
+// let result = [];
+// while (arr.length > 0) {
+//   let first = arr.shift();
+//   let last = arr.pop();
+//   let str = first + last;
+//   result.push(str);
+// }
+// result = result.join("-");
+// console.log(result);
+
+// Метод slice вырезает и возвращает указанную часть массива
+// массив.slice(откуда отрезать, докуда отрезать)
+// let arr = ["a", "b", "c", "d", "e"];
+// let sub = arr.slice(0, 2);
+// console.log(sub);
+
+// let arr = ["a", "b", "c", "d", "e"];
+// let sub = arr.slice(1);
+// console.log(sub);
+
+// let arr = ["a", "b", "c", "d", "e"];
+// let sub = arr.slice(1, -1);
+// console.log(sub);
+
+// Метод splice удаляет или добавляет элементы в массив.
+// Удаление элемента с первого 3 штуки
+//Начиная с 1 удали 3 элемента
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.splice(1, 3);
+// console.log(arr);
+
+// выведет массив удаленных элементов
+// let arr = ["a", "b", "c", "d", "e"];
+// let del = arr.splice(1, 3);
+// console.log(del);
+
+//Начиная со 2 удалить 1 элемент, и на его место добавить 1,2,3
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.splice(2, 1, "1", "2", "3");
+// console.log(arr);
+
+//Удалили элемент d
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.splice(-2, 1);
+// console.log(arr);
+
+// Метод sort производит сортировку массива в лексикографическом порядке
+// let arr = ["Банан", "Апельсин", "Яблоко", "Манго"];
+// console.log(arr.sort());
+
+//Функция Object.keys - получает ключи объекта в виде массива
+// var obj = { a: 1, b: 2, c: 3 };
+// console.log(Object.keys(obj));
+
+//Метод reverse изменяет порядок элементов в массиве на обратный
+// let arr = ["a", "b", "c"];
 // arr.reverse();
-// console.log(arr.join("/"));
+// console.log(arr);
 
+// let arr = ["a", "b", "c"];
+// let res = arr.reverse();
+// console.log(res);
+
+// let str = "123456789";
+// let arr1 = str.split("");
 // let arr2 = arr1.reverse();
-// let arr = str.split("-");
 // let result = arr2.join("");
+// console.log(result);
 
-//Задачи для решения
-//Задача 1
-// var a = "js";
-// console.log(a.toUpperCase());
+// let str = "123456789";
+// let result = str.split("").reverse().join("");
+// console.log(result);
+
+//Задачa 1
+// var a = ["a", "b", "c"];
+// var b = [1, 2, 3];
+//Вариант 1
+// var c = a.concat(b);
+// alert(c);
+//Вариант 2
+// var c = b.concat(a);
+// alert(c);
 
 // Задача 2
-// var a = "JS";
-// console.log(a.toLowerCase());
+// var arr = ["a", "b", "c"];
+// arr.push(1, 2, 3);
+// alert(arr);
+
+// Практические задачи
+// Задача 1
+// var a = [1, 2, 3],
+//   b = [4, 5, 6],
+//   c;
+// c = a.concat(b);
+// console.log(c);
+
+// Задача 2
+// var arr = [1, 2, 3];
+// arr.reverse();
+// console.log(arr);
 
 // Задача 3
-// var a = "я учу javascript",
-//   b;
-// b = a.length;
-// console.log(b);
+// var arr = [1, 2, 3];
+// arr.push(4, 5, 6);
+// console.log(arr);
 
 // Задача 4
-// var a = "я учу javascript!",
-//   b;
-// b = a.substr(0, 1);
-// console.log(b);
-// b = a.substring(0, 2);
-// console.log(b);
-// b = a.slice(0, 2);
-// console.log(b);
+// var arr = [1, 2, 3];
+// arr.unshift(4, 5, 6);
+// console.log(arr);
 
 // Задача 5
-// var a = "я учу javascript",
-//   b;
-// b = a.indexOf("учу");
-// console.log(b);
+// var arr = ["js", "css"];
+// res = arr.shift();
+// console.log(res);
 
 // Задача 6
-// var str = "text",
-//   n = 3;
-// if (str.length == n) {
-//   console.log(str);
-// }
-// if (str.length > n) {
-//   console.log(str.slice(0, n));
-// }
+// var arr = ["js", "css", "jquery"];
+// res = arr.pop();
+// console.log(res);
 
 // Задача 7
-// var str = "Я-учу-javasript!";
-// console.log(str.replace(/-/g, "!"));
+// var arr = [1, 2, 3, 4, 5];
+// var res = arr.slice(0, 3);
+// console.log(res);
 
 // Задача 8
-// var str = "я учу javascript";
-// console.log(str.split(" "));
+// var arr = [1, 2, 3, 4, 5];
+// var res = arr.slice(2, 4);
+// console.log(res);
 
 // Задача 9
-// var str = "я учу javascript";
-// console.log(str.split(""));
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 2);
+// console.log(arr);
 
 // Задача 10
-// var date = "2025-12-31";
-// console.log(date.split("-").reverse().join("."));
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(5, 0, 2, 3, 4);
+// console.log(arr);
 
 // Задача 11
-// var arr = ["я", "учу", "javascript", "!"];
-// console.log(arr.join("+"));
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(3, 0, "a", "b", "c");
+// console.log(arr);
 
-//Задача 12
-// var str = "первую букву";
-// console.log(str.substr(0, 1).toUpperCase() + str.substr(1));
+// Задача 12
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 0, "a", "b");
+// arr.splice(6, 0, "c");
+// arr.splice(9, 0, "e");
+// console.log(arr);
 
 // Задача 13
-// var str = "я учу javascript!";
-// var arr = str.split(" ");
-// for (
-//   i = 0;
-//   i < arr.length;
-//   arr[i] = arr[i].substr(0, 1).toUpperCase() + arr[i].substr(1), i++
-// );
+// var arr = [3, 4, 1, 2, 7];
+// arr.sort();
 // console.log(arr);
 
 // Задача 14
-// var str = "var_test_text";
-// var arr = str.split("_");
-// for (
-//   i = 1;
-//   i < arr.length;
-//   arr[i] = arr[i].substr(0, 1).toUpperCase() + arr[i].substr(1), i++
-// );
-// arr = arr.join("");
-// console.log(arr);
+// var obj = { js: "test", jq: "hello", css: "world" };
+// console.log(Object.keys(obj));
