2f074622eec4f763d5234d75034d247a465681e1
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-07 20:43:15 +0300

    11

--- script.js
+++ script.js
@@ -1,32 +1,96 @@
-// Задача 1
-// var str = "hello";
-// str = str[0].toUpperCase() + str.substr(1);
-// alert(str);
+// Флаги
 
-// 2 вариант решения
-// var str = "Hello";
-// str = str.split("");
-// str[0] = str[0].toUpperCase();
-// var result = str.join("");
-// alert(result);
+// var arr = ["a", "b", "c", "d", "e"];
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] == "c") {
+//     alert("Есть");
+//   }
+// }
 
-// Задача 2
-// var str = "123456";
-// str = str.split("").reverse().join("");
-// alert(str);
+// var arr = ["a", "b", "c", "d", "c"];
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] == "c") {
+//     alert("Есть");
+//   }
+// }
 
-//Задача 3
-// var str = "http://phphtml";
-// if (str.substr(0, 7) == "http://") {
+// var arr = ["a", "b", "c", "d", "e"];
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] == "c") {
+//     alert("Есть");
+//     break;
+//   }
+// }
+
+// var arr = ["a", "b", "c", "d", "c"];
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] == "c") {
+//     alert("Есть");
+//   } else {
+//     alert("Нет");
+//   }
+// }
+
+// var arr = ["a", "b", "c", "d", "e"];
+// var flag = false;
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] == "c") {
+//     flag = true;
+//     break;
+//   }
+// }
+// if (flag === true) {
+//   alert("Есть");
+// } else {
+//   alert("Нет");
+// }
+
+// function hasElem(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == "c") {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// var arr = ["a", "b", "c", "d", "c"];
+// alert(hasElem(arr));
+
+//Задача 1
+// var arr = [1, 2, 3, 4, 5];
+// var flag = false;
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] == 5) {
+//     flag = true;
+//     break;
+//   }
+// }
+// if ((flag = true)) {
 //   alert("Да");
 // } else {
-//   alert("нет");
+//   alert("Нет");
 // }
 
-// Задача 4
-// var str = "http://file.html";
-// if (str.substr(-5) == ".html") {
-//   alert("да");
-// } else {
-//   alert("нет");
+//Задача 2
+// var char = 31;
+// var flag = false;
+// for (var i = 2; i < char; i++) {
+//   if (char % i == 0) {
+//     alert("true");
+//     break;
+//   } else {
+//     alert("false");
+//     break;
+//   }
 // }
+
+// Задача 3
+// let arr = [1, 2, 3, 5, 4, 4];
+// function arrChar(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] == arr[i + 1]) return alert("да");
+//   }
+//   return alert("нет");
+// }
+
+// arrChar(arr);
