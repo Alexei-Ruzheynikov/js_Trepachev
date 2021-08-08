2d69a87325dca4ca800a7d9b37d8a93772d6cdaf
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-08 20:51:30 +0300

    14

--- script.js
+++ script.js
@@ -1,101 +1,149 @@
-// for (var i = 1; i <= 9; i++) {
-//   for (var j = 1; j <= 3; j++) {
-//     document.write(i);
-//   }
-// }
-// Стандарные общепринятые названия для счетчиков - i, j, k
+//Интересные конструкции, слегка сложные для повторения, но понять можно
 
-// var str = "";
+// var arr = [];
 // for (var i = 0; i < 10; i++) {
-//   str = str + "x";
+//   arr.push("x");
 // }
-// alert(str);
+// console.log(arr);
 
-// var str = "";
+// var arr = [];
+// for (var i = 1; i <= 10; i++) {
+//   arr.push(i);
+// }
+// console.log(arr);
+
+// var arr = [];
 // for (var i = 0; i < 10; i++) {
-//   str = str + "x";
-//   document.write(str + "<br>");
+//   arr[i] = "x";
 // }
+// console.log(arr);
 
-// var str = "";
-// for (var i = 1; i <= 9; i++) {
-//   str = str + i;
+// var arr = [];
+// for (var i = 0; i < 10; i++) {
+//   arr[i] = i + 1;
 // }
-// alert(str);
+// console.log(arr);
 
-// for (var i = 1; i <= 9; i++) {
-//   for (var j = 1; j <= 3; j++) {
-//     document.write(i);
+// var arr = ["a", "b", "c", "d", "e"];
+// var result = [];
+// for (var i = arr.length - 1; i >= 0; i--) {
+//   result.push(arr[i]);
+// }
+// console.log(result);
+
+// var obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+// var result = {};
+// for (var key in obj) {
+//   result[obj[key]] = key;
+// }
+// console.log(result);
+
+// var arr = ["a", "b", "c", "a", "a", "b"];
+// var count = { a: 0, b: 0, c: 0 };
+// for (var i = 0; i < arr.length; i++) {
+//   count[arr[i]]++;
+// }
+// console.log(count);
+
+// var arr = ["a", "b", "c", "a", "a", "b"];
+// var count = [];
+// for (var i = 0; i < arr.length; i++) {
+//   if (count[arr[i]] === undefined) {
+//     count[arr[i]] = 1;
+//   } else {
+//     count[arr[i]]++;
 //   }
-//   document.write("<br>");
 // }
+// console.log(count);
 
-// for (var i = 1; i <= 9; i++) {
-//   for (var j = 1; j <= i; j++) {
-//     document.write(i);
+// var arr = [
+//   [1, 2, 3, 4, 5],
+//   [6, 7, 8],
+//   [9, 10],
+// ];
+// for (var i = 0; i < arr.length; i++) {
+//   for (var j = 0; j < arr[i].length; j++) {
+//     alert(arr[i][j]);
 //   }
-//   document.write("<br>");
 // }
-
-// for (var i = 1; i <= 9; i++) {
-//   var str = "";
-//   for (var j = 1; j <= i; j++) {
-//     str = str + i;
-//   }
-//   document.write(str + "<br>");
-// }
-
-// var num = 500;
-// while (num > 10) {
-//   num = num / 2;
-// }
-// alert(num);
-
-// for (var num = 500; num > 10; num = num / 2);
-// alert(num);
 
 // Задача 1
+// var arr = [];
 // var str = "";
-// for (var i = 1; i <= 9; i++) {
-//   str += i;
+// for (var i = 0; i < 10; i++) {
+//   str += "x";
+//   arr.push(str);
 // }
-// document.write(str);
+// document.write(arr);
 
 //Задача 2
-// var str = "";
-// for (var i = 9; i >= 1; i--) {
-//   str += i;
+// var arr = [];
+// var char;
+// for (var i = 1; i < 10; i++) {
+//   char = "";
+//   for (var j = 1; j <= i; j++) {
+//     char += i;
+//   }
+//   arr.push(char);
 // }
-// document.write(str);
+// document.write(arr);
 
 //Задача 3
-// var str = "";
-// for (i = 1; i <= 9; i++) {
-//   str += "-" + i;
-// }
-// str += "-";
-// document.write(str);
-
-//Задача 4
-// for (var i = 1; i <= 20; i++) {
-//   for (var j = 1; j <= i; j++) {
-//     document.write("x");
+// function arrayFill(value, length) {
+//   var arr = [];
+//   for (var i = 0; i < length; i++) {
+//     arr.push(value);
 //   }
-//   document.write("<br>");
+//   return arr;
 // }
+// document.write(arrayFill("x", 5));
+
+//задача 4
+// var arr = [1, 2, 3, 4, 5, 6];
+// var sum = 0;
+// for (var i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+//   if (sum > 10) {
+//     break;
+//   }
+// }
+// document.write(i + 1);
 
 //Задача 5
-
-// for (var i = 1; i <= 9; i++) {
-//   for (var j = 1; j <= i; j++) {
-//     document.write(i);
-//   }
-//   document.write("<br>");
+// var arr = [1, 2, 3, 4, 5, 6];
+// var result = [];
+// for (var i = arr.length - 1; i >= 0; i--) {
+//   result.push(arr[i]);
 // }
+// document.write(result);
 
 //Задача 6
-// var str = "";
-// for (var i = 1; i <= 5; i++) {
-//   str = str + "xx";
-//   document.write(str + "<br>");
+// var arr = [[1, 2, 3], [4, 5], [6]];
+// var sum = 0;
+// for (var i = 0; i < arr.length; i++) {
+//   for (var j = 0; j < arr[i].length; j++) {
+//     sum += arr[i][j];
+//   }
 // }
+// alert(sum);
+
+// Задача 7
+// var arr = [
+//   [
+//     [1, 2],
+//     [3, 4],
+//   ],
+//   [
+//     [5, 6],
+//     [7, 8],
+//   ],
+// ];
+// var sum = 0;
+// for (var i = 0; i < arr.length; i++) {
+//   for (var j = 0; j < arr[i].length; j++) {
+//     for (var k = 0; k < arr[i][j].length; k++) {
+//       sum += arr[i][j][k];
+//     }
+//   }
+// }
+// document.write(sum);
