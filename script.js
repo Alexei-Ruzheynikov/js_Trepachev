5760e7eadd6a7f76f56467e3f72c50b498fcea42
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-09 18:46:18 +0300

    16

--- script.js
+++ script.js
@@ -1,171 +1,129 @@
-// function getDigits(num) {
-//   var str = String(num);
-//   return str.split("");
-// }
-
-// function getDigits(num) {
-//   return String(num).split("");
-// }
-// console.log(getDigits(123));
-
-// function arraySum(arr) {
-//   var sum = 0;
+// function inArray(value, arr) {
 //   for (var i = 0; i < arr.length; i++) {
-//     sum += Number(arr[i]);
-//   }
-//   return sum;
-// }
-// var sum = arraySum(getDigits(123));
-// alert(sum);
-
-// function inRange(num){
-//     var sum = arraySum(getDigits(num));
-//     if (sum >= 1 && sum <<= 9){
-//         return true;
-//     } else {
-//         return false;
+//     if (arr[i] == value) {
+//       return true;
 //     }
-// }
-
-// function inRange(num) {
-//   var sum = araySum(getDigits(num));
-//   return sum >= 1 && sum <= 9;
-// }
-
-// Полный пример 1
-// var arr = [12, 19, 28, 13, 14, 345];
-// var result = [];
-// for (var i = 0; i < 10; i++) {
-//   if (inRange(arr[i])) {
-//     result.push(arr[i]);
 //   }
+//   return false;
 // }
-// console.log(result);
+// var arr = [1, 2, 3, 4, 5];
+// alert(inArray(9, arr));
 
-// function inRange(num) {
-//   var sum = arraySum(getDigits(num));
-//   return sum >= 1 && sum <= 9;
-// }
-// function getDigits(num) {
-//   return String(num).split("");
-// }
-// function arraySum(arr) {
-//   var sum = 0;
-//   for (var i = 0; i < arr.length; i++) {
-//     sum += Number(arr[i]);
+// function isSimple(num) {
+//   for (var i = 2; i < num; i++) {
+//     if (num % i == 0) {
+//       return false;
+//     }
 //   }
-//   return sum;
+//   return true;
 // }
+// alert(isSimple(14));
 
-//Не понятно
-// function getDigits(num) {
-//   return String(num).split("");
-// }
-// function arraySum(arr) {
-//   var sum = 0;
-//   for (var i = 0; i < arr.length; i++) {
-//     sum += Number(arr[i]);
-//   }
-//   return sum;
-// }
-// var arr = [12, 19, 28, 13, 14, 345];
-// var sum = 0;
-// for (var i = 0; i < 10; i++) {
-//   sum += arraySum(getDigits(num));
-// }
-// alert(sum);
-
-// Задача 1
-// var arr = [1, 2, 3, -1, -2, -3];
-// function isPositive(num) {
-//   if (num >= 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// var newArr = [];
-// for (var i = 0; i < arr.length; i++) {
-//   if (isPositive(arr[i])) {
-//     newArr.push(arr[i]);
-//   }
-// }
-// console.log(newArr);
-
-// Задача 1
-// function inNumberinRange(num) {
-//   return num > 0 && num < 10;
-// }
-// alert(inNumberinRange(3));
-
-//Задача 2
-// var arr = [1, 2, 3, 4, 5, 6];
-// var result = [];
-// function isNumberinRange(num) {
-//   return num > 0 && num < 10;
-// }
-// for (i = 0; i < arr.length; i++) {
-//   if (isNumberinRange(arr[i])) {
-//     result.push(arr[i]);
-//   }
-// }
-// alert(result);
-
-//Задача 3
-// function getDigitsSum(num) {
-//   var sum = 0;
-//   var num = String(num);
-//   for (var i = 0; i < num.length; i++) {
-//     sum += Number(num[i]);
-//   }
-//   return sum;
-// }
-// alert(getDigitsSum(443));
-
-//Задача 4
-// function getDigitsSum(num) {
-//   var sum = 0;
-//   var num = String(num);
-//   for (var i = 0; i < num.length; i++) {
-//     sum += Number(num[i]);
-//   }
-//   return sum;
-// }
-// var arr = [];
-// for (i = 1; i <= 2021; i++) {
-//   if (getDigitsSum(i) == 13) {
-//     arr.push(i);
-//   }
-// }
-// alert(arr);
-
-// Задача 5
-// function isEven(num) {
-//   return num % 2 == 0;
-// }
-// alert(isEven(5));
-
-//Задача 6
-// function isEven(num) {
-//   return num % 2 == 0;
-// }
-// var arr = [1, 2, 3, 4, 5, 6];
-// var neewArr = [];
-// for (i = 0; i < arr.length; i++) {
-//   if (isEven(arr[i])) {
-//     neewArr.push(arr[i]);
-//   }
-// }
-// alert(neewArr);
-
-//Задача 7
 // function getDivisors(num) {
-//   var arr = [];
+//   var result = [];
 //   for (var i = 1; i <= num; i++) {
 //     if (num % i == 0) {
-//       arr.push(i);
+//       result.push(i);
 //     }
 //   }
-//   return arr;
+//   return result;
 // }
-// alert(getDivisors(13));
+// alert(getDivisors(24));
+
+// function inArray(value, arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// function getIntersection(arr1, arr2) {
+//   var result = [];
+//   for (var i = 0; i < arr1.length; i++) {
+//     if (inArray(arr1[i], arr2)) {
+//       result.push(arr1[i]);
+//     }
+//   }
+//   return result;
+// }
+// alert(getIntersection([1, 2, 3], [2, 3, 4]));
+
+//Пример не отработал
+// function getDivisors(num) {
+//   var result = [];
+//   for (var i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//       result.push(i);
+//     }
+//   }
+//   return result;
+// }
+// function getIntersection(arr1, arr2) {
+//   var result = [];
+//   for (var i = 0; i < arr1.length; i++) {
+//     if (inArray(arr1[i], arr2)) {
+//       result.push(arr1[i]);
+//     }
+//   }
+//   return result;
+// }
+// function inArray(value, arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// function getGreatestCommonDivisor(num1, num2) {
+//   var divisors = getIntersection(getDivisors(num1), getDivisors(num2));
+//   return Number(Max.apply(null, divisors));
+// }
+// alert(getGreatestCommonDivisor(12, 18));
+
+// Задача 1
+
+// var str = "one two three";
+// var arr = str.split(" ");
+// var newArr = [];
+// for (var i = 0; i < arr.length; i++) {
+//   newArr.push(ucfirst(arr[i]));
+// }
+// var newStr = newArr.join(" ");
+// alert(newStr);
+// function ucfirst(str) {
+//   return str[0].toUpperCase() + str.slice(1);
+// }
+
+//Задача 2
+// var str = "var_text_hello";
+// var arr = str.split("_");
+// for (var i = 1; i < arr.length; i++) {
+//   arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+// }
+// var newStr = arr.join("");
+// alert(newStr);
+
+// Задача 3
+// function inArray(value, arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// var arr = ["one", "two", "three"];
+// alert(inArray("one", arr));
+
+// Задача 4
+// function reverseNumber(str) {
+//   var arr = str.split("");
+//   var newArr = [];
+//   for (var i = 1; i < arr.length; i += 2) {
+//     newArr.push(arr[i] + arr[i - 1]);
+//   }
+//   return newArr.join("");
+// }
+// document.write(reverseNumber("123456"));
