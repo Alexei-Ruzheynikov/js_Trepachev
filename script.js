c5393a6e8202a402591feda447671a1de27d1e92
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-14 13:21:13 +0300

    21

--- script.js
+++ script.js
@@ -1,234 +1,379 @@
-// function func() {
-// var elem = document.getElementById("test");
-// elem.innerHTML = "!";
-// Так же можно добавлять тэги
-// elem.innerHTML = "<b>!</b>";
-// elem.outerHTML = "<b>!</b>";
-// }
-//
+// var date = new Date();
 
-// function func() {
-//   var elems = document.getElementsByTagName("p");
-// elems[0].innerHTML = "1";
-// elems[1].innerHTML = "2";
-// elems[2].innerHTML = "3";
-//   for (var i = 0; i < elems.length; i++) {
-//     elems[i].innerHTML = "!";
+// alert(date.getMilliseconds());
+// alert(date.getSeconds());
+// alert(date.getMinutes());
+// alert(date.getHours());
+// alert(date.getDate());
+// alert(date.getMonth());
+// alert(date.getFullYear());
+
+// alert(date.getDay());
+
+// var days = ["вс", "Пн", "вт", "ср", "чт", "пт", "сб"];
+// alert(days[2]);
+
+// var date = new Date();
+// var day = date.getDay();
+// var days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+// alert(days[day]);
+
+// var date = new Date(2025, 10, 5, 12, 59, 59);
+// var day = date.getDay();
+// var days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+// alert(days[day]);
+
+// var date = new Date(2025, 10, 5);
+// var day = date.getDay();
+// var days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+// alert(days[day]);
+
+// var date = new Date();
+// alert(date.getTime());
+
+// var date = new Date(2015, 12, 4, 23, 59, 59);
+// alert(date.getTime());
+
+// var now = new Date();
+// var date = new Date(2015, 12, 4, 23, 59, 59);
+// var diff = now.getTime() - date.getTime();
+// alert(diff);
+
+// document.write(Date.parse("2015-05-25T12:59:59"));
+
+// var now = new Date();
+// var date = new Date(2015, 23, 4, 12, 59, 59);
+// var diff = now - date;
+// document.write(diff);
+
+// var date = new Date();
+// console.log(
+//   date.getFullYear() +
+//     "." +
+//     date.getMonth() +
+//     "." +
+//     date.getDate() +
+//     "." +
+//     date.getHours() +
+//     "." +
+//     date.getMinutes() +
+//     "." +
+//     date.getSeconds()
+// );
+//1 параметр трактуется как милисекунды от 1970 года
+// var date = new Date(111111);
+// console.log(
+//   date.getFullYear() +
+//     "." +
+//     date.getMonth() +
+//     "." +
+//     date.getDate() +
+//     "." +
+//     date.getHours() +
+//     "." +
+//     date.getMinutes() +
+//     "." +
+//     date.getSeconds()
+// );
+// console.log(date.getDay());
+
+// строковый международный формат даты 03 - 3 месяц март, в js отображается как 2 месяц, т.к. номерация начинается с 0 - "2015-03-25" и с разделителем "2015-03-25T12:55:33" и можно опускать день и месяц "2015-03"
+// var date = new Date(2015);
+// console.log(
+//   date.getFullYear() +
+//     "." +
+//     date.getMonth() +
+//     "." +
+//     date.getDate() +
+//     "." +
+//     date.getHours() +
+//     "." +
+//     date.getMinutes() +
+//     "." +
+//     date.getSeconds()
+// );
+// console.log(date.getDay());
+
+// var date1 = "2022-12-01";
+// var date2 = "2019-12-01";
+
+// var date1 = new Date(date1);
+// var date2 = new Date(date2);
+// console.log(date1 > date2);
+// console.log(date1.getTime() - date2.getTime());
+
+// var date = "2022-12-01";
+// date = new Date(date);
+// console.log(date.getTime());
+
+// var date = "2022-12-01";
+// var ms = Date.parse(date);
+// console.log(ms);
+
+// var date = "2022-01-01";
+// var arr = date.split("-");
+// console.log(arr);
+// var date = new Date(arr[0], arr[1] - 1, arr[2]);
+// console.log(date);
+
+// var month = 1;
+// var year = 2018;
+// var date = new Date(year, month + 1, 0);
+
+// console.log(date.getDate());
+
+// var year = 2017;
+// var date = new Date(year, 2, 0);
+// if (date.getDate() == 29) {
+//   console.log("Високосный");
+// } else {
+//   console.log("Не високосный");
+// }
+// console.log(date.getDate());
+
+//функция реализована - месяц начинается с 1
+// function checkDate(year, month, day) {
+//   let date = new Date(year, month - 1, day);
+//   if (
+//     date.getFullYear() == year &&
+//     date.getMonth() == month - 1 &&
+//     date.getDate() == day
+//   ) {
+//     return true;
+//   } else {
+//     return false;
 //   }
 // }
 
-//document.forms - обращаемся ко всем формам как к массиву по номеру
-// document.forms[0].innerHTML = "!";
+// console.log(checkDate(2018, 1, 32));
 
-// var forms = document.forms;
-// for (var i = 0; i < forms.length; i++) {
-//   forms[i].innerHTML = "!";
-// }
+// let date = new Date(2018, 0, 32);
+// console.log(date.getFullYear(), date.getMonth(), date.getDate());
 
-// к форме можно обращаться по атрибуту name у нее
-// document.forms.form1.innerHTML = "!";
-// document.forms["form2"].innerHTML = "?";
+// var str = "2018-12-01";
+// var date = new Date();
+// date = addZero(date.getMonth() + 1) + "-" + addZero(date.getDate());
+// console.log(date);
 
-//обращение к свойству elements формы(содержит массив свойств формы)
-// document.forms[1].elements[0].value = "!";
-
-//Переберем циклом - код не отработал как ожидалось
-// var elements = document.forms[1].elements;
-// for (var i = 0; i < elements.length; i++) {
-//   elements[i] = "!";
-// }
-
-// обращение по name формы и name инпута
-// document.forms.form1.elements.input1.value = "!";
-
-// метод getAttribute - считывает значение указанного атрибута
-// метод setAttribute - записывает новое значение в атрибут
-// метод removeAttribute - удаляет атрибут совсем
-// метод hasAttribute - проверяет наличие или отсутствие атрибута у свойства
-// function func() {
-//   var elem = document.getElementById("test");
-// alert(elem.getAttribute("value"));
-// elem.setAttribute("value", "www");
-// elem.removeAttribute("value");
-// alert(elem.hasAttribute("value"));
-// }
-
-// с помощью метода getElementsByClassName получаем свойства по классу
-// function func() {
-//   var elems = document.getElementsByClassName("www");
-//   for (var i = 0; i < elems.length; i++) {
-//     elems[i].innerHTML = "!";
+// function addZero(num) {
+//   if (num <= 9) {
+//     num = "0" + num;
 //   }
+//   return num;
+// }
+// if (date >= "08-05" && date <= "09-19") {
+//   console.log(true);
+// } else if (date >= "09-20" && date <= "10-12") {
+//   console.log(true);
 // }
 
-// С помощью метода querySelector можно найти любой элемент, в случае querySelector если их много, то выбран будет первый
-// function func() {
-// var elem = document.querySelector("input.test");
-//   var elem = document.querySelector("#test");
+// ТАЙМЕР ДО ПОЛУНОЧИ
+// var text = document.querySelector("#text");
+// timer();
+// setInterval(timer, 1000);
+// function timer() {
+//   var now = new Date();
+//   var target = new Date(
+//     now.getFullYear(),
+//     now.getMonth(),
+//     now.getDate() + 1,
+//     0,
+//     0,
+//     0
+//   );
+//   var diff = Math.ceil((target - now) / 1000);
+//   var hours = Math.floor(diff / (60 * 60));
+//   var diff = diff % (60 * 60);
 
-//   alert(elem.getAttribute("value"));
+//   var minutes = Math.floor(diff / 60);
+//   var diff = diff % 60;
+
+//   var seconds = diff;
+//   text.innerHTML =
+//     addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
 // }
-
-// С помощью метода querySelectorAll можно найти все элементы подходящие под поиск(описание)
-// function func() {
-//   var elems = document.querySelectorAll(".www");
-//   for (var i = 0; i < elems.length; i++) {
-//     elems[i].innerHTML = "!";
+// function addZero(num) {
+//   if (num <= 9) {
+//     num = "0" + num;
 //   }
+//   return num;
 // }
 
-// Мы можем создавать свои атрибуты через data-свое-название и обращаться к ним через dataset.своеНазвание
-// function func() {
-//   var elem = document.getElementById("elem");
-//   alert(elem.dataset.price);
-//   alert(elem.dataset.productNumber);
+//ТАЙМЕР ДО НОВОГО ГОДА
+// var text = document.querySelector("#text");
+// timer();
+// setInterval(timer, 1000);
+// function timer() {
+//   var now = new Date();
+//   var target = new Date(now.getFullYear() + 1, 0, 1, 0, 0, 0);
+
+//   var diff = Math.ceil((target - now) / 1000);
+
+//   var days = extract(diff, 60 * 60 * 24);
+//   var hours = extract(days.diff, 60 * 60);
+//   var minutes = extract(hours.diff, 60);
+
+//   var seconds = extract(minutes.diff, 1);
+//   text.innerHTML =
+//     addZero(days.value) +
+//     ":" +
+//     addZero(hours.value) +
+//     ":" +
+//     addZero(minutes.value) +
+//     ":" +
+//     addZero(seconds.value);
 // }
 
-// function func() {
-// var elem = document.getElementById("test");
-// elem.value = "new"; //поменяли свойство
-// alert(elem.getAttribute("value")); //выведет 'old' - не изменилось!
+// function extract(diff, formula) {
+//   var value = Math.floor(diff / formula);
+//   var diff = diff % formula;
 
-// var elem = document.getElementById("test");
-
-// elem.setAttribute("value", "new"); //поменяли атрибут
-// alert(elem.value); //выведет 'new' - elem.value изменилось!
-// }
-//
-
-// function buttonClick() {
-//   var elem = document.getElementById("elem");
-//   elem.innerHTML = "!!!";
+//   return { value: value, diff: diff };
 // }
 
-// function buttonClick() {
-//   var elem = document.getElementById("elem");
-//   elem.outerHTML = "<b>" + elem.outerHTML + "</b>";
-// }
-
-// function func() {
-//   var elems = document.getElementsByTagName("p");
-//   for (var i = 0; i < elems.length; i++) {
-//     elems[i].innerHTML = i + 1;
+// function addZero(num) {
+//   if (num <= 9) {
+//     num = "0" + num;
 //   }
+//   return num;
 // }
 
-// function func() {
-//   var elems = document.getElementsByClassName("zzz");
-//   for (var i = 0; i < elems.length; i++) {
-//     elems[i].innerHTML = i + 1;
+// var date = new Date();
+// alert(
+//   date.getFullYear() + "-" + addZero(date.getMonth()) + "-" + date.getDate()
+// );
+// function addZero(num) {
+//   if (num <= 9) {
+//     num = "0" + num;
 //   }
+//   return num;
 // }
 
-// function func() {
-//   var elems = document.querySelectorAll("p.zzz");
-//   for (var i = 0; i < elems.length; i++) {
-//     elems[i].innerHTML = i + 1;
+// var months = [
+//   "янв",
+//   "фев",
+//   "мар",
+//   "апр",
+//   "май",
+//   "июн",
+//   "июл",
+//   "авг",
+//   "сен",
+//   "окт",
+//   "ноя",
+//   "дек",
+// ];
+// var date = new Date();
+// var month = date.getMonth();
+// alert(months[month]);
+
+//Практические задачи
+// Задача 1
+// var date = new Date();
+// alert(date.getDate());
+
+// Задача 2
+// var date = new Date();
+// alert(date.getMonth() + 1);
+
+//Задача 3
+// var date = new Date();
+// alert(date.getFullYear());
+
+// Задача 4
+// var date = new Date();
+// alert(
+//   addZero(date.getHours()) +
+//     ":" +
+//     addZero(date.getMinutes()) +
+//     ":" +
+//     addZero(date.getSeconds()) +
+//     " " +
+//     addZero(date.getFullYear()) +
+//     "." +
+//     addZero(date.getMonth() + 1) +
+//     "." +
+//     addZero(date.getDate())
+// );
+// function addZero(num) {
+//   if (num <= 9) {
+//     num = "0" + num;
 //   }
+//   return num;
 // }
 
-// function func() {
-//   var elem = document.getElementsByClassName("test");
-//   elem[0].innerHTML = "Ку-ку! А я" + "<b>жирный</b>";
-// }
+//Задача 5
+// var date = new Date();
+// alert(date.getDay());
 
-// function func() {
-//   var elem = document.getElementsByTagName("p");
-//   elem[0].outerHTML = "<h3>Абзац превратился в h3!</h3>";
+// Задача 6
+// var date = new Date();
+// var day = date.getDay();
+// function showDay(day) {
+//   var days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+//   return days[day];
 // }
+// alert(showDay(day));
 
-// function func() {
-//   var elem = document.getElementsByTagName("p");
-//   elem[0].outerHTML =
-//     "<h3>При нажатии на кнопку абзац станет h3, но текст останется</h3>";
+//Задача 7
+// var date = new Date(2015, 0, 7);
+// var day = date.getDay();
+// function showDay(day) {
+//   var days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+//   return days[day];
 // }
+// alert(showDay(day));
 
-// function func() {
-//   var elem = document.getElementsByTagName("input");
-//   var Value1 = Number(elem[0].value);
-//   var Value2 = Number(elem[1].value);
-//   var elemResult = document.getElementsByClassName("result");
-//   elemResult[0].innerHTML = Value1 + Value2;
-// }
+// Задача 8
+// var date = new Date();
+// var minute = Math.floor(date.getTime() / (1000 * 60));
+// alert(minute);
 
-// function func() {
-//   var elem = document.getElementsByTagName("p");
-//   for (i = 0; i < elem.length; i++) {
-//     elem[i].innerHTML = "Ку-ку";
+// Задача 9
+// var time = Date.parse("1988-03-01T00:00:00");
+// var date = new Date();
+// var now = date.getTime();
+// var result = now - time;
+// result = result / (1000 * 60 * 60);
+// alert(result);
+
+//Задача 10
+// var date = new Date();
+// var now = new Date(
+//   date.getFullYear(),
+//   date.getMonth(),
+//   date.getDate(),
+//   0,
+//   0,
+//   0
+// );
+// var result = Math.floor((date.getTime() - now.getTime()) / 1000);
+// alert(result);
+
+//Задача 11
+// var date = new Date();
+// var now = new Date(
+//   date.getFullYear(),
+//   date.getMonth(),
+//   date.getDate() + 1,
+//   0,
+//   0,
+//   0
+// );
+// var result = Math.floor((now.getTime() - date.getTime()) / 1000);
+// alert(result);
+
+// Задача 12
+//Сложная задача, пока не понимаю.
+// var inp = document.getElementById("inp");
+// var p = document.getElementById("test");
+// inp.addEventListener("blur", function () {
+//   var arr = this.value.split("-");
+//   var bd = new Date(arr[0], arr[1] - 1, arr[2]);
+//   var now = new Date();
+//   bd.setFullYear(now.getFullYear());
+//   if (bd - now < 0) {
+//     bd.setFullYear(bd.getFullYear() + 1);
 //   }
-// }
-
-// var elem = document.getElementsByClassName("www");
-// for (var i = 0; i < elem.length; i++) {
-//   elem[i].innerHTML = i + 1;
-// }
-
-// var elem = document.querySelectorAll("p.www");
-// for (var i = 0; i < elem.length; i++) {
-//   elem[i].innerHTML = i + 1;
-// }
-
-// function func() {
-//   var elem = document.querySelector("#my-class");
-//   alert(elem.getAttribute("class"));
-// }
-
-// function func() {
-//   var elem = document.querySelector("#my-class");
-//   alert(elem.getAttribute("class"));
-// }
-// function func2() {
-//   var elem = document.querySelector("#my-class");
-//   elem.removeAttribute("class");
-// }
-
-// function func() {
-//   var elem = document.querySelector("#my-class");
-//   alert(elem.getAttribute("class"));
-// }
-// function func2() {
-//   var elem = document.querySelector("#my-class");
-//   elem.setAttribute("class", "new-class");
-//   alert("Класс измменен, нажмите на 1 кнопку для првоерки");
-// }
-
-// function func() {
-//   var elem = document.getElementById("input1");
-//   var elemTwo = document.getElementById("p1");
-//   var Value1 = elem.value;
-//   elemTwo.innerHTML = elem.value;
-
-// document.getElementById("p1").innerHTML =
-//   document.getElementById("input1").value;
-// }
-
-// function func() {
-//   var links = document.getElementsByTagName("a");
-//   for (var i = 0; i < links.length; i++) {
-//     links[i].innerHTML += "(" + links[i].getAttribute("href") + ")";
-//   }
-// }
-
-// function func() {
-//   var elem = document.getElementById("p1");
-//   elem.innerHTML = "Привет, мир!";
-//   elem.setAttribute("class", "my-class");
-//   var elemTwo = document.getElementById("input1");
-//   elemTwo.value = "О на меня нельзя нажать";
-//   elemTwo.disabled = true;
-// }
-
-// function func() {
-//   var elem = document.getElementsByClassName("p1");
-//   for (var i = 0; i < elem.length; i++) {
-//     elem[i].innerHTML = i + 1;
-//   }
-// }
-
-function func() {
-  var elem = document.getElementsByClassName("p1");
-
-  for (var i = 0; i < elem.length; i++) {
-    elem[i].innerHTML = i + 1 + "." + elem[i].innerHTML;
-  }
-}
+//   p.innerHTML = Math.floor((bd - now) / (1000 * 60 * 60 * 24)) + "дней";
+// });
